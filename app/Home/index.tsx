5f04ca6b4104597d77fba6e93232e20607c6943a
alihshaikh <alihshaikh322@gmail.com>
2025-07-18 12:06:36 -0700

    added some config, installed nativewind, made mock up home page and some routing

--- app/Home/index.tsx
+++ app/Home/index.tsx
@@ -1,41 +1,65 @@
-import { useState } from "react";
 import { Text, View } from "react-native";
 
 import SmartButton from "@/components/smartbutton/SmartButton";
+import { Colors } from "@/constants/Colors";
+import { useRouter } from "expo-router";
+import LottieView from "lottie-react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
+  const router = useRouter();
+
   return (
     // flex-1 takes full screen height
-    <SafeAreaView className="flex-1 bg-gray-50">
+    <SafeAreaView
+      className="flex-1"
+      style={{ backgroundColor: Colors.light.background }}
+    >
       <View className="flex-row justify-between mt-4 mx-2 ">
         <Text className="text-3xl font-bold ">
           <Text>Project</Text>
           <Text className="text-green-800">May🍎</Text>
         </Text>
       </View>
-      <View className="flex-1 flex-col justify-between">
+      <View className="flex-1 flex-col justify-center">
         <View>
-          <View className="flex flex-row justify-center mt-24 mx-4">
-            <Text className="text-3xl ">
+          <View className="flex flex-row justify-center mt-16 mx-4">
+            <Text className="text-3xl" style={{ color: Colors.light.text }}>
               <Text>Get instant nutrition facts 🔎</Text>
             </Text>
           </View>
           <View className="flex flex-row justify-center mx-4">
-            <Text className="text-3xl ">
+            <Text className="text-3xl " style={{ color: Colors.light.text }}>
               <Text>Explore what's in your food 🧐</Text>
             </Text>
           </View>
           <View className="flex flex-row justify-center mx-4">
-            <Text className="text-3xl ">
+            <Text className="text-3xl " style={{ color: Colors.light.text }}>
               <Text>Transform your eating habits 💪🏼</Text>
             </Text>
           </View>
         </View>
-        <View></View>
+        <View className="flex flex-row justify-center">
+          <LottieView
+            autoPlay
+            loop={false}
+            style={{ width: 450, height: 460 }}
+            source={require("../../assets/pantryapp.json")}
+          />
+        </View>
         <View className="flex flex-col items-center">
-          <SmartButton title="Scan Barcode" className="w-96 mb-2" />
-          <SmartButton title="Login with Google" className="w-96" />
+          <SmartButton
+            title="Scan Barcode"
+            className="w-96 mb-2"
+            onPress={() => router.push("/scan")}
+            // colorMode= dark | light
+          />
+          <SmartButton
+            title="Login with Google"
+            className="w-96"
+            onPress={() => router.push("/google-login")}
+            // colorMode= dark | light
+          />
         </View>
       </View>
     </SafeAreaView>
--- components/smartbutton/SmartButton.tsx
+++ components/smartbutton/SmartButton.tsx
@@ -1,18 +1,36 @@
+import { Colors } from "@/constants/Colors";
 import { Pressable, Text } from "react-native";
+
+type colorMode = "light" | "dark";
 
 type Props = {
   title: string;
   onPress: () => void;
   className?: string;
+  colorMode: colorMode;
 };
 
-export default function SmartButton({ title, onPress, className = "" }: Props) {
+export default function SmartButton({
+  title,
+  onPress,
+  className = "",
+  colorMode = "dark",
+}: Props) {
+  const colorSet = Colors[colorMode];
   return (
     <Pressable
       onPress={onPress}
-      className={`px-8 py-4 rounded-3xl bg-green-700 ${className}`}
+      className={`px-8 py-4 rounded-3xl  ${className}`}
+      style={{
+        backgroundColor: colorSet.background,
+      }}
     >
-      <Text className="text-white text-center font-semibold">{title}</Text>
+      <Text
+        className="text-center font-semibold"
+        style={{ color: colorSet.text }}
+      >
+        {title}
+      </Text>
     </Pressable>
   );
 }
